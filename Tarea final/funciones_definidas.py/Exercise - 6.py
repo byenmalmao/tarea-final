8a2e86c26afb448b7209451aba410516b7e4d4f3
Enmanuel Bautista <148397318+byenmalmao@users.noreply.github.com>
2023-11-28 14:30:10 -0400

    Delete Tarea final directory

--- Tarea final/funciones_definidas.py/Exercise - 6.py
+++
@@ -1,9 +0,0 @@
-def contar_palabras_unicas(cadena_texto):
-    cadena = cadena_texto.lower()
-    palabras = cadena.split()
-    palabras_unicas = set(palabras)
-
-    return len(palabras_unicas)
-
-
-print(contar_palabras_unicas(input("Ingrese una oracion: ")))
